78ef53071287297c651c0c4ff9fb3f5eee31b536
Amit Shrestha <amit.p.shrestha@gmail.com>
2020-11-29 19:09:52 -0800

    Bump up stash version.

--- stash.rb
+++ stash.rb
@@ -1,19 +1,20 @@
 class Stash < Formula
   desc "A command-line utility for encrypted text storage."
   homepage "https://github.com/rorokimdim/stash"
-  version "0.1.0"
+  version "0.2.0"
 
   if OS.linux?
-    url "https://github.com/rorokimdim/stash/releases/download/v0.1.0/stash-x86_64-linux.zip"
-    sha256 "ff691edbe79532816002ebd3d2063195e14fa5ab957d8e1a15839a722e9b7915"
+    url "https://github.com/rorokimdim/stash/releases/download/v0.2.0/stash-x86_64-linux.zip"
+    sha256 "af3b7ac52188d7d5f10ff28dd5fc852f0cbd41437edf0d64bd28acde1afebe47"
   else
-    url "https://github.com/rorokimdim/stash/releases/download/v0.1.0/stash-mac-os.zip"
-    sha256 "c72dceb026d4033bd06580f139e241c7d25ab0f011c908f9d378e15730dc7092"
+    url "https://github.com/rorokimdim/stash/releases/download/v0.2.0/stash-mac-os.zip"
+    sha256 "8db4568dcecf78dccb396c56c0ed942689aced3ad2a0df31b452d349675968b0"
   end
 
   bottle :unneeded
 
   def install
     bin.install "stash"
+    man1.install "stash.1"
   end
 end
