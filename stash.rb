c0fb04ec85ea3741cc54d4ac3fb9b9f52cdd2a37
Amit Shrestha <amit.p.shrestha@gmail.com>
2024-10-29 09:24:54 -0700

    Bump up stash version.

--- stash.rb
+++ stash.rb
@@ -1,15 +1,20 @@
 class Stash < Formula
   desc "A command-line utility for encrypted text storage."
   homepage "https://github.com/rorokimdim/stash"
-  version "0.3.3"
+  version "0.3.4"
   license "BSD-3-Clause"
 
   if OS.linux?
-    url "https://github.com/rorokimdim/stash/releases/download/v0.3.3/stash-Linux.zip"
-    sha256 "a4840e9f1c57948ce77fa7625783232833407f28e7a74cc28678b0d28438e1f9"
+    url "https://github.com/rorokimdim/stash/releases/download/v0.3.4/stash-ubuntu-latest.zip"
+    sha256 "17cb12c0061255a3271acb8aaa892ed5d73d73ab6997d73f5b7212579225c347"
   else
-    url "https://github.com/rorokimdim/stash/releases/download/v0.3.3/stash-macOS.zip"
-    sha256 "0c167cf05afc0ea2a5bffefb2f11b0551aaa9443e8dec023aeac75af8ad99f4a"
+    if Hardware::CPU.arm?
+      url "https://github.com/rorokimdim/stash/releases/download/v0.3.4/stash-macOS-latest.zip"
+      sha256 "97a96b4f63c398bcc35bce2da7750b6f3f12c4670cc01c2bb78c89a41c219ce5"
+    else
+      url "https://github.com/rorokimdim/stash/releases/download/v0.3.4/stash-macOS-13.zip"
+      sha256 "c8b2335c55ac971c2f1c286f18eff07b4c4c767372fff1591f3dcbe8067eb33a"
+    end
   end
 
   def install
